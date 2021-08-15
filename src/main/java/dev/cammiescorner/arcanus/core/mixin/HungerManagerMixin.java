f54a5fcec3ea087c01f74df6490ad15f33d3f30f
CammiePone <inazuma261@gmail.com>
2021-08-14 21:31:26 -0400

    new weakness of burnout

---
+++ src/main/java/dev/cammiescorner/arcanus/core/mixin/HungerManagerMixin.java
@@ -0,0 +1,17 @@
+package dev.cammiescorner.arcanus.core.mixin;
+
+import dev.cammiescorner.arcanus.core.util.MagicUser;
+import net.minecraft.entity.player.HungerManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.GameRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(HungerManager.class)
+public class HungerManagerMixin {
+	@ModifyVariable(method = "update", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$Key;)Z", ordinal = 0))
+	public boolean hasBurnout(boolean bl, PlayerEntity player) {
+		return ((MagicUser) player).getBurnout() <= 0 && player.world.getGameRules().getBoolean(GameRules.NATURAL_REGENERATION);
+	}
+}
