0645fc9f25dab69b04e7ddeffc794c9081fa8a40
CammiePone <inazuma261@gmail.com>
2021-08-19 21:00:40 -0400

    despair

--- src/main/java/dev/cammiescorner/arcanus/core/mixin/client/MouseMixin.java
+++ src/main/java/dev/cammiescorner/arcanus/core/mixin/client/MouseMixin.java
@@ -3,7 +3,6 @@ package dev.cammiescorner.arcanus.core.mixin.client;
 import dev.cammiescorner.arcanus.core.util.CanBeDiscombobulated;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
-import net.minecraft.client.util.SmoothUtil;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,22 +14,6 @@ public abstract class MouseMixin {
 	@Shadow
 	@Final
 	private MinecraftClient client;
-	@Shadow
-	@Final
-	private SmoothUtil cursorXSmoother;
-	@Shadow
-	@Final
-	private SmoothUtil cursorYSmoother;
-	@Shadow
-	private double lastMouseUpdateTime;
-	@Shadow
-	private double cursorDeltaX;
-	@Shadow
-	private double cursorDeltaY;
-
-	@Shadow
-	public abstract boolean isCursorLocked();
-
 	@ModifyArg(method = "updateMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;changeLookDirection(DD)V"), index = 0)
 	public double changeLookDirectionX(double x) {
 		if(client.player instanceof CanBeDiscombobulated player && player.isDiscombobulated())
