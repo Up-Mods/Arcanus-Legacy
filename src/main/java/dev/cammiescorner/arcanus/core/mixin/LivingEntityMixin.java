64e16b1232237a89d189355af33541af57b3a190
CammiePone <inazuma261@gmail.com>
2021-10-11 15:29:34 -0400

    removed air strafing attribute

--- src/main/java/dev/cammiescorner/arcanus/core/mixin/LivingEntityMixin.java
+++ src/main/java/dev/cammiescorner/arcanus/core/mixin/LivingEntityMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,8 +23,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity implements CanBeDiscombobulated {
-	@Shadow private LivingEntity attacking;
-
 	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
