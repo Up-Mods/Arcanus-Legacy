d2b26f2f9c7493df7368012baba10d9256cf7840
CammiePone <inazuma261@gmail.com>
2022-01-26 21:52:35 -0500

    fixed solar strike crash

--- src/main/java/dev/cammiescorner/arcanus/core/registry/ModDamageSource.java
+++ src/main/java/dev/cammiescorner/arcanus/core/registry/ModDamageSource.java
@@ -1,8 +1,12 @@
 package dev.cammiescorner.arcanus.core.registry;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.EntityDamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class ModDamageSource extends DamageSource {
 	public static final DamageSource MAGIC_BURNOUT = new ModDamageSource("magic_burnout").setBypassesArmor().setUnblockable();
@@ -11,7 +15,16 @@ public class ModDamageSource extends DamageSource {
 		super(name);
 	}
 
-	public static EntityDamageSource solarStrike(Entity attacker) {
-		return (EntityDamageSource) new EntityDamageSource("solar_strike", attacker).setUsesMagic();
+	public static DamageSource solarStrike(Entity attacker) {
+		return new EntityDamageSource("solar_strike", attacker) {
+			@Override
+			public Text getDeathMessage(LivingEntity entity) {
+				Text text = attacker == null ? new TranslatableText("entity.arcanus.solar_strike") : attacker.getDisplayName();
+				ItemStack itemStack = attacker instanceof LivingEntity living ? living.getMainHandStack() : ItemStack.EMPTY;
+				String string = "death.attack." + this.name;
+				String string2 = string + ".item";
+				return !itemStack.isEmpty() && itemStack.hasCustomName() ? new TranslatableText(string2, entity.getDisplayName(), text, itemStack.toHoverableText()) : new TranslatableText(string, entity.getDisplayName(), text);
+			}
+		}.setUsesMagic();
 	}
 }
