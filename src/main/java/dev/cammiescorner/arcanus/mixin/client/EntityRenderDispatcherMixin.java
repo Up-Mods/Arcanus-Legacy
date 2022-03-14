e73c0c40f40e8b942ab6919e124882bca353eaa1
williambl <william@williambl.com>
2022-03-14 19:31:21 +0000

    Add aura rendering using AuraEffectManager

--- src/main/java/dev/cammiescorner/arcanus/mixin/client/EntityRenderDispatcherMixin.java
+++ src/main/java/dev/cammiescorner/arcanus/mixin/client/EntityRenderDispatcherMixin.java
@@ -1,68 +1,51 @@
 package dev.cammiescorner.arcanus.mixin.client;
 
-import dev.cammiescorner.arcanus.client.AuraShaderManager;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
+import dev.cammiescorner.arcanus.client.AuraEffectManager;
+import dev.cammiescorner.arcanus.common.registry.ArcanusComponents;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3f;
-import net.minecraft.util.math.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import static dev.cammiescorner.arcanus.Arcanus.id;
-
 @Mixin(EntityRenderDispatcher.class)
 public abstract class EntityRenderDispatcherMixin {
-    @Shadow public Camera camera;
-
     @SuppressWarnings("InvalidInjectorMethodSignature")
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderer;render(Lnet/minecraft/entity/Entity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"), locals = LocalCapture.CAPTURE_FAILHARD)
     private <E extends Entity> void renderAuraForEntity(E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci, EntityRenderer<E> entityRenderer, Vec3d vec3d) {
-        this.renderAura(matrices, vertexConsumers, entity, entityRenderer, tickDelta, light);
+        float aura = this.getAuraFor(entity);
+        if (aura > 0) {
+            this.renderAura(matrices, vertexConsumers, entity, entityRenderer, tickDelta, light, aura, this.getAuraColourFor(entity));
+        }
     }
 
-    //FIXME: behaves a little oddly with vertical angles
     @Unique
-    private <E extends Entity> void renderAura(MatrixStack matrices, VertexConsumerProvider vertexConsumers, E entity, EntityRenderer<E> entityRenderer, float tickDelta, int light) {
-        float scale = 1.5F;
+    private float getAuraFor(Entity entity) {
+        return ArcanusComponents.AURA_COMPONENT.isProvidedBy(entity) ? ArcanusComponents.AURA_COMPONENT.get(entity).getAura() / (float) ArcanusComponents.AURA_COMPONENT.get(entity).getMaxAura() : 0;
+    }
+
+    @Unique
+    private int getAuraColourFor(Entity entity) {
+        return ArcanusComponents.SPELL_COMPONENT.isProvidedBy(entity) ? ArcanusComponents.SPELL_COMPONENT.get(entity).getSelectedSpell().getSpellType().getDecimal() : 0;
+    }
+
+    @Unique
+    private <E extends Entity> void renderAura(MatrixStack matrices, VertexConsumerProvider vertexConsumers, E entity, EntityRenderer<E> entityRenderer, float tickDelta, int light, float aura, int auraColour) {
+        float scale = 0.5F + aura;
 
         matrices.push();
 
-        matrices.translate(0.0, -entity.getHeight()*0.25, 0.0);
         matrices.scale(scale, scale, scale);
 
-        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-this.camera.getYaw()));
-        matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(this.camera.getPitch()));
-
-        matrices.scale(1f, 1f, 0f);
-
-        matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(-this.camera.getPitch()));
-        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(this.camera.getYaw()));
-
-        Vector4f transformedEntityPos = new Vector4f(new Vec3f(entity.getPos().withAxis(Direction.Axis.Y, entity.getBodyY(0.5))));
-
-        //transformedEntityPos.transform(matrices.peek().getPositionMatrix());
-
-        AuraShaderManager.setEntityValues(transformedEntityPos.getX(), transformedEntityPos.getY(), transformedEntityPos.getZ(), entity.getWidth(), entity.getHeight());
-
-        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(AuraShaderManager.AURA_SHADER.getRenderLayer(RENDER_LAYER));
-
-        entityRenderer.render(entity, 0, tickDelta, matrices, layer -> vertexConsumer, light);
+        entityRenderer.render(entity, 0, tickDelta, matrices, layer -> vertexConsumers.getBuffer(AuraEffectManager.INSTANCE.getRenderLayer(layer)), 0xffffffff);
 
         matrices.pop();
     }
-
-    private static final RenderLayer RENDER_LAYER = RenderLayer.getEntityTranslucent(id("textures/aura.png"));
 }
