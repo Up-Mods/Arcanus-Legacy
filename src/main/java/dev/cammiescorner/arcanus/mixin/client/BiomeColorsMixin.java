1b2545b72ba055046f5636bcc67c12ffb7579a6f
CammiePone <inazuma261@gmail.com>
2022-04-19 12:15:25 -0400

    fix purble water with Sodium

--- src/main/java/dev/cammiescorner/arcanus/mixin/client/BiomeColorsMixin.java
+++ src/main/java/dev/cammiescorner/arcanus/mixin/client/BiomeColorsMixin.java
@@ -3,28 +3,23 @@ package dev.cammiescorner.arcanus.mixin.client;
 import dev.cammiescorner.arcanus.common.components.chunk.PurpleWaterComponent;
 import dev.cammiescorner.arcanus.common.registry.ArcanusComponents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.block.FluidRenderer;
+import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
-@Mixin(value = FluidRenderer.class, priority = 9999)
-public class FluidRendererMixin {
-	@ModifyVariable(method = "render", at = @At(value = "CONSTANT",
-			args = "intValue=16",
-			ordinal = 0,
-			shift = At.Shift.BEFORE
-	), ordinal = 0)
-	public int arcanus$baterWucket(int colour, BlockRenderView world, BlockPos pos) {
+@Mixin(value = BiomeColors.class, priority = 9999)
+public class BiomeColorsMixin {
+	@Inject(method = "getWaterColor", at = @At("HEAD"), cancellable = true)
+	private static void arcanus$baterWucket(BlockRenderView world, BlockPos pos, CallbackInfoReturnable<Integer> info) {
 		Optional<PurpleWaterComponent> optional = ArcanusComponents.PURPLE_WATER_COMPONENT.maybeGet(MinecraftClient.getInstance().world.getChunk(pos)).filter(component -> component.isPosNearAltar(pos.toImmutable()));
 
 		if(optional.isPresent())
-			return 0x8408e3;
-		else
-			return colour;
+			info.setReturnValue(0x8408e3);
 	}
 }
