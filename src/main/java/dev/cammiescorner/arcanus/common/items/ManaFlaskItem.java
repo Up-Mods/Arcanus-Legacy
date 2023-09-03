bf8854ab3e37de3b101a8a2e88125fef9cf168bb
Up <upcraftedlp@gmail.com>
2023-09-03 13:09:20 +0200

    make mana flask emit drinking game event on use

--- src/main/java/dev/cammiescorner/arcanus/common/items/ManaFlaskItem.java
+++ src/main/java/dev/cammiescorner/arcanus/common/items/ManaFlaskItem.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
@@ -15,96 +16,97 @@ import net.minecraft.text.Text;
 import net.minecraft.util.*;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class ManaFlaskItem extends Item {
-	public ManaFlaskItem() {
-		super(new FabricItemSettings().maxCount(1));
-	}
+    public ManaFlaskItem() {
+        super(new FabricItemSettings().maxCount(1).food(new FoodComponent.Builder().alwaysEdible().build()));
+    }
 
-	@Override
-	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-		if(!world.isClient() && user instanceof PlayerEntity player && player instanceof MagicUser magicUser) {
-			NbtCompound tag = stack.getOrCreateSubNbt(Arcanus.MOD_ID);
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (!world.isClient() && user instanceof PlayerEntity player && player instanceof MagicUser magicUser) {
+            NbtCompound tag = stack.getOrCreateSubNbt(Arcanus.MOD_ID);
 
-			if(player.isSneaking() && tag.getInt("Mana") < 4 && magicUser.getMana() >= 5) {
-				if(!player.isCreative()) {
-					magicUser.addMana(-5);
-					magicUser.setLastCastTime(world.getTime());
-				}
+            if (player.isSneaking() && tag.getInt("Mana") < 4 && magicUser.getMana() >= 5) {
+                if (!player.isCreative()) {
+                    magicUser.addMana(-5);
+                    magicUser.setLastCastTime(world.getTime());
+                }
 
-				tag.putInt("Mana", tag.getInt("Mana") + 1);
-			}
-			else if(tag.getInt("Mana") > 0){
-				if(!player.isCreative()) {
-					magicUser.addMana(MathHelper.clamp((magicUser.getMaxMana() - magicUser.getBurnout()) - magicUser.getMana(), 0, 5));
-					magicUser.setLastCastTime(world.getTime());
-				}
+                tag.putInt("Mana", tag.getInt("Mana") + 1);
+            } else if (tag.getInt("Mana") > 0) {
+                if (!player.isCreative()) {
+                    magicUser.addMana(MathHelper.clamp((magicUser.getMaxMana() - magicUser.getBurnout()) - magicUser.getMana(), 0, 5));
+                    user.emitGameEvent(GameEvent.DRINK);
+                    magicUser.setLastCastTime(world.getTime());
+                }
 
-				tag.putInt("Mana", tag.getInt("Mana") - 1);
-				tag.putBoolean("Filling", false);
-			}
-		}
+                tag.putInt("Mana", tag.getInt("Mana") - 1);
+                tag.putBoolean("Filling", false);
+            }
+        }
 
-		return stack;
-	}
+        return stack;
+    }
 
-	@Override
-	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-		NbtCompound tag = user.getStackInHand(hand).getOrCreateSubNbt(Arcanus.MOD_ID);
-		MagicUser magicUser = (MagicUser) user;
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        NbtCompound tag = user.getStackInHand(hand).getOrCreateSubNbt(Arcanus.MOD_ID);
+        MagicUser magicUser = (MagicUser) user;
 
-		tag.putBoolean("Filling", user.isSneaking() && tag.getInt("Mana") < 4 && magicUser.getMana() >= 5);
+        tag.putBoolean("Filling", user.isSneaking() && tag.getInt("Mana") < 4 && magicUser.getMana() >= 5);
 
-		if(tag.getBoolean("Filling") || (magicUser.getMana() < magicUser.getMaxMana() && tag.getInt("Mana") > 0))
-			return ItemUsage.consumeHeldItem(world, user, hand);
-		else
-			return TypedActionResult.fail(user.getStackInHand(hand));
-	}
+        if (tag.getBoolean("Filling") || (magicUser.getMana() < magicUser.getMaxMana() && tag.getInt("Mana") > 0))
+            return ItemUsage.consumeHeldItem(world, user, hand);
+        else
+            return TypedActionResult.fail(user.getStackInHand(hand));
+    }
 
-	@Override
-	public boolean isItemBarVisible(ItemStack stack) {
-		int mana = stack.getOrCreateSubNbt(Arcanus.MOD_ID).getInt("Mana");
-		return mana > 0;
-	}
+    @Override
+    public boolean isItemBarVisible(ItemStack stack) {
+        int mana = stack.getOrCreateSubNbt(Arcanus.MOD_ID).getInt("Mana");
+        return mana > 0;
+    }
 
-	@Override
-	public int getItemBarStep(ItemStack stack) {
-		int mana = stack.getOrCreateSubNbt(Arcanus.MOD_ID).getInt("Mana");
-		return Math.round((mana * 13F) / 4F);
-	}
+    @Override
+    public int getItemBarStep(ItemStack stack) {
+        int mana = stack.getOrCreateSubNbt(Arcanus.MOD_ID).getInt("Mana");
+        return Math.round((mana * 13F) / 4F);
+    }
 
-	@Override
-	public int getItemBarColor(ItemStack stack) {
-		return 0x3BB3CB;
-	}
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        return 0x3BB3CB;
+    }
 
-	@Override
-	public UseAction getUseAction(ItemStack stack) {
-		NbtCompound tag = stack.getSubNbt(Arcanus.MOD_ID);
-		return tag == null || tag.getBoolean("Filling") ? UseAction.BOW : UseAction.DRINK;
-	}
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        NbtCompound tag = stack.getSubNbt(Arcanus.MOD_ID);
+        return tag == null || tag.getBoolean("Filling") ? UseAction.BOW : UseAction.DRINK;
+    }
 
-	@Override
-	public String getTranslationKey(ItemStack stack) {
-		NbtCompound tag = stack.getSubNbt(Arcanus.MOD_ID);
+    @Override
+    public String getTranslationKey(ItemStack stack) {
+        NbtCompound tag = stack.getSubNbt(Arcanus.MOD_ID);
 
-		if(tag == null || tag.getInt("Mana") <= 0)
-			return Util.createTranslationKey("item", Registries.ITEM.getId(this)) + "_empty";
+        if (tag == null || tag.getInt("Mana") <= 0)
+            return Util.createTranslationKey("item", Registries.ITEM.getId(this)) + "_empty";
 
-		return super.getTranslationKey(stack);
-	}
+        return super.getTranslationKey(stack);
+    }
 
-	@Override
-	public int getMaxUseTime(ItemStack stack) {
-		return 32;
-	}
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 32;
+    }
 
-	@Override
-	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-		super.appendTooltip(stack, world, tooltip, context);
-		tooltip.add(Text.translatable(stack.getTranslationKey()+".description").formatted(Formatting.DARK_AQUA));
-	}
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(Text.translatable(stack.getTranslationKey() + ".description").formatted(Formatting.DARK_AQUA));
+    }
 }
